5aa2fe694189d5d87622db9c2aa1ee164a991de3
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-06-04 16:47:01 +0200

    2D array cubes

--- cubeUnit.js
+++ cubeUnit.js
@@ -2,8 +2,8 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 scene.add(camera);
-var renderer = new THREE.WebGLRenderer();
-// renderer.setClearColor( 0xffffff, 1.0 );
+var renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setClearColor( 0xffffff, 0.0 );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -12,7 +12,10 @@ controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.campingFactor = 0.25;
 controls.enableZoom = true;
-camera.position.z = 10;
+// controls.enableKeys = false;
+// controls.enablePan = false;
+// controls.enableRotate = false;
+camera.position.z = 15;
 
 var _testMesh;
 var _normalMat = new THREE.MeshNormalMaterial();
@@ -22,11 +25,40 @@ var matCapCube = new THREE.MeshMatcapMaterial({matcap:texCube});
 matCapCube.visible = false;
 var _redMat = new THREE.MeshBasicMaterial({color:0xff0000});
 var _testGeo = new THREE.CubeGeometry( 5, 5, 5);
-var _spacingX = 8;
-var _spacingY = 2;
+var _spacingX = 20;
+var _spacingY = 10;
 var isActive = false;
 var countDiv = document.querySelector('h1');
-var countString = countDiv.innerHTML;
+var countString = "39811";
+var numbIsDisp = false;
+ var _cubesArray = [];
+
+//UI
+var datGUI = new dat.GUI();
+var guiControls = new function () {
+  this.cameraPosX = camera.position.x;
+  this.cameraPosY = camera.position.y;
+  this.cameraPosZ = camera.position.z;
+  this.spacingX = 20;
+}
+
+var cameraPosition = datGUI.addFolder('CameraPos');
+
+cameraPosition.add(guiControls, 'cameraPosX', -50, 100 ).onChange(function(value) {
+  camera.position.x = value;
+});
+cameraPosition.add(guiControls, 'cameraPosY', -50, 100 ).onChange(function(value) {
+  camera.position.y = value;
+});
+cameraPosition.add(guiControls, 'cameraPosZ', -50, 100 ).onChange(function(value) {
+  camera.position.z = value;
+});
+
+datGUI.add(guiControls, 'spacingX', 0, 50).onChange(function(value) {
+  _spacingX = value;
+})
+
+
 
 window.addEventListener("DOMContentLoaded", (event) => {
   init();
@@ -38,7 +70,6 @@ function init() {
   _testMesh = new THREE.Mesh(_testGeo, _normalMat);
   // scene.add(_testMesh);
   generateCubes();
-
   render();
 }
 
@@ -80,21 +111,35 @@ function cubeDisp() {
 
 }
 
+
+function dispNumber () {
+   countDiv.innerHTML="39811";
+   numbIsDisp = true;
+   // matCapCube.visible = true;
+}
+
+function dispCountry () {
+   countDiv.innerHTML="United Kingdom";
+   numbIsDisp = false;
+   // matCapCube.visible = false;
+}
 function generateCubes() {
   var numberOfDigits = countString.length;
-  // var count = parseInt(countString, 10);
-  // console.log(numberOfDigits);
 
   for(var i = 0; i<numberOfDigits; i++) {
     var number = parseInt(countString[i]);
-    console.log(number);
+    var _rowCubes = [];
     for(var j = 0; j < number; j++) {
-      var cubeGeo = new THREE.CubeGeometry(i, i, i);
+      var cubeGeo = new THREE.CubeGeometry(12-i, 12-i, 12-i);
       var cube = new THREE.Mesh(cubeGeo, matCapCube);
-      cube.position.y = j*(i+ _spacingY) ;
+      cube.position.z = j*(i+ _spacingY) ;
       cube.position.x = i*_spacingX;
       scene.add(cube);
+      _rowCubes[j] = cube;
 
     }
+    _cubesArray[i] = _rowCubes;
   }
+
+  console.log(_cubesArray);
 }

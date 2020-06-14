53dd85d422294b31c2b20c1a8b5b679dd521df20
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-06-14 16:52:36 +0200

    pixel grid divs done

--- pixelGrid.js
+++ pixelGrid.js
@@ -1,9 +1,21 @@
-var container = document.getElementById('pixelGrid');
+var container = document.getElementById('pixelGridContainer');
 var gridSize = 10;
+var cellSize = 49;
 
-for(var i =0; i< gridSize; i++ ) {
-  for(var j = 0; j < gridSize; j++) {
-    var div = document.createElement('div');
-    div.classList.add('cell');
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log(container);
+  generateGrid();
+});
+
+function generateGrid() {
+  for(var i =0; i< gridSize; i++ ) {
+    for(var j = 0; j < gridSize; j++) {
+      var div = document.createElement('div');
+      div.classList.add('cell');
+      div.style.left = cellSize*i + "px";
+      div.style.top = cellSize*j + "px";
+      div.style.opacity = Math.random();
+      container.appendChild(div);
+    }
   }
 }

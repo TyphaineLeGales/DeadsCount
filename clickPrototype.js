7ba12005306a0dd5e90b32d9b6b24480953a0f8d
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-06-15 19:10:16 +0200

    count

--- clickPrototype.js
+++ clickPrototype.js
@@ -9,9 +9,9 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 //Camera
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.campingFactor = 0.25;
+// controls = new THREE.OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.campingFactor = 0.25;
 // controls.enableZoom = true;
 // controls.enableKeys = false;
 // controls.enablePan = false;
@@ -30,14 +30,17 @@ var _cubeGroup = new THREE.Group();
 var _cubesArray = [];
 var currIndex = 0;
 var _animationCubeTime = 2;
-var _offsetPositionStart = -12.5;
+var _offsetPositionStart = -13;
 var _animNext = false;
 var _animPrev= false;
+var _count = 0;
 
 //Background
 var backgroundTexSky = new THREE.TextureLoader().load( 'Assets/skyTest2.jpg' );
 var backgroundTexBlack = new THREE.TextureLoader().load( 'Assets/gradientB&W.jpg' );
 scene.background = backgroundTexBlack;
+
+var countContainer = document.querySelector('h1.count');
 
 window.addEventListener("DOMContentLoaded", (event) => {
   init();
@@ -77,14 +80,14 @@ function generateCubes () {
   var length = _placeholderString.length;
   var spacing = 2;
   var scaleY = 1;
-
+  var unit = 1;
   for(var i =0; i < length; i++) {
-
     var currNum = parseInt(_placeholderString[i]);
     for(var j = 0; j < currNum; j++) {
       var mat = new THREE.MeshMatcapMaterial({matcap:texCube, transparent: true});
       mat.needsUpdate = true;
       var mesh = new THREE.Mesh(geo, mat);
+      mesh.userData.unit = unit;
       scaleY = length-i;
       mesh.scale.y += scaleY;
       mesh.position.set(_offsetPositionStart, scaleY/2, 0);
@@ -92,14 +95,16 @@ function generateCubes () {
       scene.add(mesh);
       _cubesArray.push(mesh);
     }
+    unit *= 10;
   }
 }
 
 function animNavNext (dt) {
    var currUnit = _cubesArray[currIndex];
     _animTimer += dt;
-    currUnit.position.x += 0.1;
-    currUnit.material.opacity = mapRange(_animTimer, 0, _animationCubeTime, 0, 1 );
+    var t = mapRange(_animTimer, 0, _animationCubeTime, 0, 1 );
+    currUnit.position.x  = mapRange(_animTimer, 0, _animationCubeTime, _offsetPositionStart, 0 );
+    currUnit.material.opacity = ease(t);
 
     if(_cubesArray[currIndex-1]) {
       var prevUnit = _cubesArray[currIndex-1];
@@ -133,25 +138,38 @@ function animNavPrev(dt) {
 
 
 function next () {
-  if(currIndex < _cubesArray.length-1) {
-    currIndex += 1;
-    _cubesArray[currIndex].visible = true;
-    _animNext = true;
-  }
-  console.log(currIndex);
+  if(_animNext === false && _animPrev === false) {
 
+    if(currIndex < _cubesArray.length-1) {
+      currIndex += 1;
+      _count += _cubesArray[currIndex].userData.unit;
+      _cubesArray[currIndex].visible = true;
+      _animNext = true;
+    }
+  }
+  console.log(_count);
+  countContainer.innerHTML = _count;
 }
 
 function prev () {
-  if(currIndex > 0) {
-    currIndex -= 1;
-    _cubesArray[currIndex].visible = true;
-    _animPrev = true;
+  if(_animNext === false && _animPrev === false) {
+    if(currIndex > 0) {
+      currIndex -= 1;
+      _count -= _cubesArray[currIndex].userData.unit;
+      _cubesArray[currIndex].visible = true;
+      _animPrev = true;
+    }
   }
-  console.log(currIndex);
+  console.log(_count);
+  countContainer.innerHTMTL = _count;
 }
 
 function mapRange(value, a, b, c, d) {
   value = (value - a) / (b - a);
   return c + value*(d-c);
+}
+
+function ease (t) {
+    sqt = t * t;
+    return sqt / (2*(sqt-t)+1);
 }

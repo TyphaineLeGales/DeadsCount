f092c9f4390f240d51066bf131fe4d53b2f89577
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-06-19 16:55:30 +0200

    call linearAnim on main gui

---
+++ linearAnimation.js
@@ -0,0 +1,77 @@
+const _MAXOBJ = 10;
+var _straightPath = [];
+let totalCount = 0;
+const _OPACITYTHRESHOLDIN = 0.1;
+const _OPACITYTHRESHOLDOUT = 0.9;
+var _progressWidth;
+
+function linearAnimationInit() {
+
+  createObj();
+  straightPath();
+  _splinePath = new Spline(_straightPath);
+}
+
+function linearAnimationRender(scene, camera, maxCount, speed, progressBar) {
+
+  if(totalCount < maxCount) {
+    _f = (dt*speed)%1;
+
+    //progressBar
+    _progressWidth = mapRange(totalCount, 0,maxCount, 0, 50);
+    progressBar.style.width = _progressWidth + "%";
+
+    for(var i = 0; i<_unitArray.length; i++) {
+      var obj = _unitArray[i];
+      obj.userData.f = ((_f + obj.userData.offset))%1;
+      _splinePath.setObjectPath(obj, obj.userData.f);
+      obj.lookAt(camera.position);
+      opacityEase(obj.userData.f, obj);
+      if(obj.userData.f< obj.userData.prevF ) {
+        totalCount += 1;
+      }
+      obj.userData.prevF = obj.userData.f;
+    }
+  }
+  // requestAnimationFrame( render );
+  // renderer.render( scene, camera );
+}
+
+function createObj() {
+ for(var i = 0; i<_MAXOBJ; i++) {
+  var redMat = new THREE.MeshBasicMaterial({color:0xff0000, transparent:true});
+  redMat.needsUpdate = true;
+  redMat.opacity = 0;
+  var startPos = new THREE.Vector3(-5, 0, 0);
+  var obj = new THREE.Mesh(new THREE.CircleGeometry(0.2, 32), redMat);
+  obj.userData.offset = i*0.1;
+  obj.userData.f = 0;
+  obj.userData.number = i;
+  obj.userData.prevF = 0;
+  obj.position.copy(startPos);
+  scene.add(obj);
+  _unitArray.push(obj);
+
+  }
+}
+
+function straightPath () {
+  var vec3Array = [];
+  for(var i = 0; i < 10; i++) {
+    var point = new THREE.Vector3(i-5, 0, 0);
+    _straightPath.push(i-5, 0, 0)
+    vec3Array.push(point);
+  }
+}
+
+function opacityEase(f, obj) {
+
+  if(f < _OPACITYTHRESHOLDIN) {
+    obj.material.opacity =  mapRange(f, 0,_OPACITYTHRESHOLDIN, 0, 1);
+  } else if (f > _OPACITYTHRESHOLDOUT) {
+    obj.material.opacity = mapRange(f,_OPACITYTHRESHOLDOUT,1, 1, 0);
+  } else {
+    obj.material.opacity = 1;
+  }
+}
+
--- main.js
+++ main.js
@@ -1,84 +1,126 @@
-var number = 0;
-var systemSelected = "linearAnimation";
+let number = 0;
+let systemSelectedStr = "linearAnimation";
+let numberHeader = document.querySelector('h1.numberHeader');
+const countDiv = document.querySelector('h1.count');
+const progressBar = document.getElementById('progress');
 
+let _unitArray = [];
+var clock = new THREE.Clock(); //units a second
+var dt = 0;
+
+var scene, camera, renderer ;
+
+window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener("DOMContentLoaded", (event) => {
+  init();
+  render();
+});
+
+function init() {
+  guiControls.linearAnimation = true;
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  scene.add(camera);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  var backgroundTexBlack = new THREE.TextureLoader().load( '../Assets/gradientB&W.jpg' );
+  scene.background = backgroundTexBlack;
+  //linearAnim
+}
+
+
+function render () {
+  dt += clock.getDelta();
+  if(systemSelectedStr === "linearAnimation") {
+    linearAnimationRender(scene, camera, guiControls.number, guiControls.speed, progressBar);
+  }
+  requestAnimationFrame( render );
+
+  renderer.render( scene, camera );
+};
+
+
+//utility functions
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+function mapRange(value, a, b, c, d) {
+  value = (value - a) / (b - a);
+  return c + value*(d-c);
+}
+
+function  clamp ( value, min, max ) {
+
+  return Math.max( min, Math.min( max, value ) );
+
+}
+
+//UI
 var datGUI = new dat.GUI();
 var guiControls = new function () {
-  this.linearAnimation = true;
+  this.linearAnimation = false;
   this.cubeFractal = false;
   this.clickInteraction = false;
   this.scrollInteraction = false;
-  this.number = 0;
-  this.speed = 0;
+  this.number = 593;
+  this.speed = 1;
 }
 
 var typeOfVis = datGUI.addFolder('typeOfVisualization');
 
 typeOfVis.add(guiControls, "linearAnimation").listen().onChange(function(value){
   if(value) {
-    systemSelected = "linearAnimation";
+    systemSelectedStr = "linearAnimation";
+    linearAnimationInit();
+    console.log(scene);
     guiControls.cubeFractal = false;
     guiControls.clickInteraction = false;
     guiControls.scrollInteraction = false;
   }
-  logSystemSelected();
 })
 
 typeOfVis.add(guiControls, "cubeFractal").listen().onChange(function(value){
   if(value) {
-    systemSelected = "cubeFractal";
+    systemSelectedStr = "cubeFractal";
     guiControls.linearAnimation = false;
     guiControls.clickInteraction = false;
     guiControls.scrollInteraction = false;
   }
-   logSystemSelected();
 })
 
 typeOfVis.add(guiControls, "clickInteraction").listen().onChange(function(value){
   if(value) {
-    systemSelected = "clickInteraction";
+    systemSelectedStr = "clickInteraction";
     guiControls.linearAnimation = false;
     guiControls.cubeFractal = false;
     guiControls.scrollInteraction = false;
   }
-   logSystemSelected();
 })
 
 typeOfVis.add(guiControls, "scrollInteraction").listen().onChange(function(value){
   if(value) {
-    systemSelected = "scrollInteraction";
+    systemSelectedStr = "scrollInteraction";
     guiControls.linearAnimation = false;
     guiControls.cubeFractal = false;
     guiControls.clickInteraction = false;
   }
-   logSystemSelected();
 })
 
 datGUI.add(guiControls, 'speed', 1, 10, 1);
 
-datGUI.add(guiControls, 'number').step(1).onChange(function(value) {
+datGUI.add(guiControls, 'number').min(0).step(1).onChange(function(value) {
 
   // header.innerHTML = value;
-  number = value;
+  // number = value;
   numberHeader.innerHTML = value;
 
 })
 
-function logSystemSelected() {
-
-  switch(systemSelected) {
-
-    case "linearAnimation" : console.log(systemSelected);
-    break;
-
-    case "cubeFractal" : console.log(systemSelected);
-    break;
-
-    case "clickInteraction" : console.log(systemSelected);
-    break;
-
-    case "scrollInteraction" : console.log(systemSelected);
-    break;
-  }
-}
 
 
